3a989827f1da092a89359aef70421d7266e96336
Omayeli Arenyeka <oa495@nyu.edu>
2015-11-07 15:04:26 -0500

    adding images + grid

--- Flatline-p5/public/scripts/sketch.js
+++ Flatline-p5/public/scripts/sketch.js
@@ -1,26 +1,27 @@
-
+var cols = 0;
+var rows = 0;
+var gridScale = 8;
 var stuff = 0;
 var thetweet = 'hi there';
-var socket = io.connect();
-socket.on('stuff', function(msg){
-  stuff = msg;
-});
-socket.on('tweet', function(msg){
-  console.log(msg);
-  if(msg!=undefined) thetweet = msg;
-});
-
 
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight);
+  cols = width/gridScale;
+  rows = height/gridScale;
+  for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+        // Scaling up to draw a rectangle at (x,y)
+        var x = i*gridScale;
+       	var y = j*gridScale;
+        fill(109, 197, 170);
+        stroke(255);
+        strokeWeight(0.5);
+        rect(x, y, gridScale, gridScale);
+      }
+    }
 }
 
 function draw() {
-//  console.log(stuff);
-  background(255);
-  ellipse(width/2, height/2, stuff, stuff);
-  text(thetweet, 50, 50);
+  ellipse(width/2, height/2, 100, 100);
 }
-
-
