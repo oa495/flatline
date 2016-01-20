8e504302f96ff2a1c2432b8756b519c5a81c8240
Omayeli Arenyeka <aomayeli@gmail.com>
2016-01-20 14:35:08 -0500

    all settings in one page

--- Flatline-p5/server.js
+++ Flatline-p5/server.js
@@ -42,8 +42,8 @@ var express = require('express');
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-//var InstagramStrategy = require('passport-instagram').Strategy;
-//var TumblrStrategy = require('passport-tumblr').Strategy;
+var InstagramStrategy = require('passport-instagram').Strategy;
+var TumblrStrategy = require('passport-tumblr').Strategy;
 var Twitter = require('twitter');
 var ig = require('instagram-node-lib');
 
@@ -61,7 +61,7 @@ var twitClient = new Twitter({
 });
 
 /** TUMBLR STUFF **/
-/*
+
 passport.use(new TumblrStrategy({
     consumerKey: '711YLRai4CmlGhtUZIa2vLmGbAYVdnxOKkaT0Cp4MbgfeG5lqX',
     consumerSecret: 'xb9nqFCIJyJpL3OnkEP2M3B4QI4rOy41Cx9pHGC0MU3G5wJVGi',
@@ -77,10 +77,10 @@ passport.use(new TumblrStrategy({
       return done(null, profile);
     });
   }
-)); */
+)); 
 
 /** INSTAGRAM STUFF **/
-/*
+
 passport.use(new InstagramStrategy({
     clientID: 'b8ea3dcb540743fc9d1b92110261002e',
     clientSecret: '6696c3641a554af88bcf0a61657094f5',
@@ -97,7 +97,8 @@ function(accessToken, refreshToken, profile, done) {
       return done(null, profile);
     });
   }
-));*/
+));
+
 /** FACEBOOK STUFF **/
 
 /** SERVER **/
@@ -144,14 +145,14 @@ app.get('/start', function(req, res, next) {
     if (twitter || insta || tumblr) {
       res.render('start');
   }
-  else res.redirect('settings');
+  else res.redirect('setup');
 });
 
 app.get('/visualize', function(req, res, next) {
   if (twitter || insta || tumblr) {
     res.render('visualize');
   }
-  else res.redirect('/settings');
+  else res.redirect('/setup');
 });
 
 app.post('/start', function(req, res, next) {
@@ -165,11 +166,11 @@ app.get('/about', function(req, res, next) {
   res.render('about');
 });
 
-app.get('/settings', function(req, res, next) {
+app.get('/setup', function(req, res, next) {
   res.render('settings');
 });
 
-app.post('/settings', function(req, res, next) {
+app.post('/setup', function(req, res, next) {
   if (!req.body.twitterUsername && !req.body.instaUsername && !req.body.tumblrUsername) {
       console.log('no values entered');
   }
@@ -188,7 +189,7 @@ app.post('/settings', function(req, res, next) {
 
 
 
-/*
+
 app.get('/auth/instagram',
 	passport.authenticate('instagram'),
 	function(req, res){
@@ -202,21 +203,21 @@ app.get('/auth/instagram',
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/instagram/callback', 
-	passport.authenticate('instagram', { failureRedirect: '/settings' }),
+	passport.authenticate('instagram', { failureRedirect: '/setup' }),
 	function(req, res) {
-	res.redirect('/settings');
+	res.redirect('/setup');
 });
 
 app.get('/auth/tumblr',
   passport.authenticate('tumblr'));
 
 app.get('/auth/tumblr/callback', 
-  passport.authenticate('tumblr', { failureRedirect: '/settings' }),
+  passport.authenticate('tumblr', { failureRedirect: '/setup' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/settings');
+    res.redirect('/setup');
   });
-*/
+
 
 server.listen(app.get('port'));
 
